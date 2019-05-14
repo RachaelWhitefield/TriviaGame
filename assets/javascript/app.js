917e57b0c70c41073eb375ec783b85cfd7e581e0
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-05-14 12:31:58 -0600

    added var arrays for questions, possible answers, and correct answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+})
+
+var questionArray = [ "Where does everyone live?", "What does Spongebob live in?", "What is Mr. Krabs' first name?", "Who is Mermaid Man's sidekick?", "Who is always trying to steal the Krabby Patty formula?", "Who is Spongebob's driving instructor?", "Who always yells, 'My leg?'" ];
+
+var answerArray = [ 
+    ["Shell City", "Bubble City", "Rock Bottom", "Bikini Bottom"],
+    ["Anchor", "Pineapple", "Tree Dome", "Easter Island Head"],
+    ["Gary", "Eugene", "Patchy", "Larry"],
+    ["The Quickster", "Captain Magma", "Barnicle Boy", "The Elastic Waistband"],
+    ["Plankton", "ManRay", "The Flying Dutchman", "Old Man Jenkins"],
+    ["Sandy Cheeks", "Perch Perkins", "Karen", "Mrs. Puff"],
+    ["Patrick", "Bubble Bass", "Fred", "Squilliam Fancyson"],
+]
+
+var correctAnswers =
+    [   "Bikini Bottom",
+        "Pineapple",
+        "Eugene",
+        "Barnicle Boy",
+        "Plankton",
+        "Mrs. Puff",
+        "Fred"];
+                
+
+// NEED //
+    // 1 MINUTE COUNTDOWN TIMER
+    // CORRECT/INCORRECT LAST PAGE
+    // OPTIONS BUTTONS
+
+
+// Where does everyone live?
+    // Bikini Bottom - Shell City - Rock Bottom - Bubble City
+
+// What is Spongebob's house?
+    // Pineapple - Anchor - Easter Island Head - Tree Dome
+
+// What is Mr. Krabs' first name?
+    // Gary - Eugene - Patchy - Larry
+
+// Who is Mermaid Man's sidekick?
+    // The Quickster - Barnicle Boy - Captain Magma - The Elastic Waistband
+
+// Who is always trying to steal the Krabby Patty formula?
+    // ManRay - Plankon - The Flying Dutchman - Old Man Jenkins
+
+// Who is Spongebob's Driving Teacher?
+    // Sandy Cheeks - Mrs. Puff - Perch Perkins - Karen
+
+// Who always yells, "My leg!"?
+    // Fred - Patrick - Bubble Bass - Squilliam Fancyson
