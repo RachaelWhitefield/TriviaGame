69a9dece1dd2d2e8d16fd715d8eb1a5de1cd94a9
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-05-17 10:58:23 -0600

    added score html and variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,72 +1,9 @@
 $(document).ready(function() {
-    var questionCounter = 0;
-    var time = 15;
-    var correctAnswers = 0;
-    var incorrectAnswers = 0;
-    var unansweredQuestions = 0;
-    
-
-
-
-// function questionContent() {
-//     var questionText = "<p>" +
-//     questions[questionCounter].question + 
-//     "</p><p class='choices'>" +
-//     questions[questionCounter].choices[0] +
-//     "</p><p class='choices'>" +
-//     questions[questionCounter].choices[1] +
-//     "</p><p class='choices'>" +
-//     questions[questionCounter].choices[2] +
-//     "</p><p class='choices'>" + 
-//     questions[questionCounter].choices[3] +
-//     "</p>";
-//     $("#gameArea").html(questionText);
-// }
-
-// function userRight() {
-//     $("#gameArea").html("<p>Yup!</p>");
-//     rightImage;
-//     correctAnswers++;
-//     var correctAnswer = questions[questionCounter].correctAnswer;
-//     setTimeout(nextQuestion, 3000);
-//     questionCounter++;
-// }
-
-// function userWrong() {
-//     $("#gameArea").html("<p>Nope!</p>");
-//     wrongImage;
-//     incorrectAnswers++;
-//     var correctAnswer = questions [questionCounter].correctAnswer;
-//     $("gameArea").append("<p>The answer was <span class='answer'>" +
-//     correctAnswer + 
-//     "</span></p>" +
-//     questions[questionCounter].image);
-//     setTimeout(nextQuestion, 3000);
-//     questionCounter++;
-// }
-
-// function nextQuestion() {
-//     questionContent();
-// }
-// questionContent()
-// $(document).on("click", ".choices", function(){
-//     var userChoice = $(this).text();
-//     console.log($(this).text());
-
-//     if (userChoice === questions[questionCounter].correctAnswer) {
-//         console.log("Answer is right");
-//         userRight();
-//     } else {
-//         console.log("The answer is wrong");
-//         userWrong();
-//     }
-
-// })
-
-
-
-
-
-
-
-// })
+var totalQuiz = 10;
+    answers = ["Bikini Bottom", "Pineapple", "Eugene", "Barnacle Boy", "Plankton", "Mrs. Puff", "Fred Rechid", "Octopus", "Boys Who Cry", "Gary"],
+    data = $("input"),
+    correctAnswer = 0;
+    incorrectAnswer = 0;
+    blank = 0;
+    count = 10;
+})
