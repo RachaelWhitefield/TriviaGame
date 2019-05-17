0a98a56d14ff3d6e8aed3f7334b29caf95556489
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-05-17 17:30:33 -0600

    added timedown function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    var startBtn = document.getElementById("play");
             var totalQuiz = 10;
             answers = ["Bikini Bottom", "Pineapple", "Eugene", "Barnacle Boy", "Plankton", "Mrs. Puff", "Fred Rechid", "Octopus", "Boys Who Cry", "Gary"],
                 // data = $("input"),
@@ -7,8 +8,22 @@ $(document).ready(function () {
             blank = 0;
             count = 10;
 
+            function initialize() {
+                startBtn.style.display = "none";
+            }
+            var timeleft = 90;
+            var downloadTimer = setInterval(function(){
+                document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+                timeleft -= 1;
+                if(timeleft <= 0) {
+                    clearInterval(downloadTimer);
+                    document.getElementById("countdown").innerHTML = "Time's Up!"
+                }
+            }, 1000);
+
             function scoreCount() {
                 for (var i = 0; i < answers.length; i++) {
+                    console.log(answers);
                     if (answers[i].checked) {
                         if (answers.indexOf([i].value) )
                             correctAnswer++;
