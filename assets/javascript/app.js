7d38d48ca3a5759ad5d5ca7cbd82442c8bba0ea6
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-05-17 11:27:16 -0600

    fixed radio button functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,29 @@
-$(document).ready(function() {
-var totalQuiz = 10;
-    answers = ["Bikini Bottom", "Pineapple", "Eugene", "Barnacle Boy", "Plankton", "Mrs. Puff", "Fred Rechid", "Octopus", "Boys Who Cry", "Gary"],
-    data = $("input"),
-    correctAnswer = 0;
-    incorrectAnswer = 0;
-    blank = 0;
-    count = 10;
-})
+$(document).ready(function () {
+            var totalQuiz = 10;
+            answers = ["Bikini Bottom", "Pineapple", "Eugene", "Barnacle Boy", "Plankton", "Mrs. Puff", "Fred Rechid", "Octopus", "Boys Who Cry", "Gary"],
+                // data = $("input"),
+            correctAnswer = 0;
+            incorrectAnswer = 0;
+            blank = 0;
+            count = 10;
+
+            function scoreCount() {
+                for (var i = 0; i < answers.length; i++) {
+                    if (answers[i].checked) {
+                        if (answers.indexOf([i].value) )
+                            correctAnswer++;
+                    } else {
+                        incorrectAnswer++;
+                    }
+                }
+            }
+
+            var totalAnswered = correctAnswer + incorrectAnswer;
+            console.log(totalAnswered);
+            if (totalAnswered !== totalQuiz) {
+                blank = totalQuiz - totalAnswered;
+            }
+            $('#correct').html(" " + correctAnswer);
+            $('#incorrect').html(" " + incorrectAnswer);
+            $("#blank").html(" " + blank);
+        })
